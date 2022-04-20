f6e2ea186e2a0eeade7c528634d07fd0965de28e
Tomohiro Uno <uuuteenet@gmail.com>
2022-04-20 18:23:34 +0900

    Ignore .DS_Store

--- frontend/lib/posts.ts
+++ frontend/lib/posts.ts
@@ -11,7 +11,7 @@ const postsDirectory = path.join(process.cwd(), 'posts')
 
 export function getSortedPostsData() {
   // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory)
+  const fileNames = fs.readdirSync(postsDirectory).filter(fileName => fileName.endsWith('.md'))
   const allPostsData = fileNames.map(fileName => {
     // Remove ".md" from file name to get id
     const id = fileName.replace(/\.md$/, '')
