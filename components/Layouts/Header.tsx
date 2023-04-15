a0cddc7e51285d08799a7df2c27e9267d8c607e3
Tomohiro Uno <uuuteenet@gmail.com>
2023-04-15 10:12:51 +0900

    Adjust header

--- components/Layouts/Header.tsx
+++ components/Layouts/Header.tsx
@@ -4,57 +4,34 @@ import { FC } from 'react'
 
 const Header: FC = () => {
   return (
-    <header css={containerStyle}>
-      <div css={innerStyle}>
-        <h1 css={titleStyle}>
-          <Link href={'/'}>
-            <a css={linkStyle}>uuutee.log()</a>
-          </Link>
-        </h1>
-        <h2 css={subtitleStyle}>すぐ忘れるのでメモ</h2>
-      </div>
+    <header className="flex items-center justify-between py-10">
+      <h1 css={titleStyle}>
+        <Link href={'/'}>
+          <a css={linkStyle}>uuutee.log</a>
+        </Link>
+      </h1>
     </header>
   )
 }
-
-const containerStyle = css`
-  background-image: linear-gradient(to right, #243949 0, #517fa4 100%);
-`
-
-const innerStyle = css`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 10px;
-`
 
 const titleStyle = css`
   width: 200px;
   font-size: 1.8rem;
   margin: 0;
   text-align: center;
-  font-weight: 200;
   letter-spacing: -2px;
   word-break: break-word;
-  color: #fff;
 `
 
 const linkStyle = css`
-  color: #fff;
+  font-weight: 700;
+  color: #243949;
+  transition: color 0.2s ease-in-out;
   text-decoration: none;
   cursor: pointer;
   &:hover {
-    color: #ccc;
+    color: #517fa4;
   }
-`
-
-const subtitleStyle = css`
-  margin: 0;
-  font-size: 1rem;
-  text-align: center;
-  line-height: 1.5;
-  color: #fff;
-  font-weight: 400;
 `
 
 export default Header
