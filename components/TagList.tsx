436b2e3d7da16b3a0a308d81777ae4d50d5a425a
Tomohiro Uno <uuuteenet@gmail.com>
2023-04-19 11:08:24 +0900

    Center the tag

--- components/TagList.tsx
+++ components/TagList.tsx
@@ -1,4 +1,3 @@
-import { css } from '@emotion/react'
 import Link from 'next/link'
 import { FC } from 'react'
 import { Tag } from '../types'
@@ -9,24 +8,20 @@ type Props = {
 
 const TagList: FC<Props> = ({ tags = [] }: Props) => {
   return (
-    <ul className="flex max-w-lg flex-wrap" css={listStyle}>
-      {tags.map(tag => (
-        <li className="mt-2 mb-2 mr-5" key={tag.id}>
-          <span>
-            <Link href={`/tags/${tag.id}`}>
-              <a>{tag.text}</a>
-            </Link>
-          </span>
-        </li>
-      ))}
-    </ul>
+    <div className="flex justify-center">
+      <ul className="flex max-w-lg flex-wrap justify-center">
+        {tags.map(tag => (
+          <li className="mt-2 mb-2 mr-5" key={tag.id}>
+            <span>
+              <Link href={`/tags/${tag.id}`}>
+                <a>{tag.text}</a>
+              </Link>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
-
-const listStyle = css`
-  list-style: none;
-  margin: 0;
-  padding: 0 0 0 10px;
-`
 
 export default TagList
