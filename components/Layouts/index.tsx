fdd466d4124dd86002bccb837c746ea29b3f1bf5
Tomohiro Uno <uuuteenet@gmail.com>
2023-04-22 09:42:21 +0900

    Append site title

--- components/Layouts/index.tsx
+++ components/Layouts/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useMemo } from 'react'
 import { SITE_TITLE } from '../../lib/constants'
 import Footer from './Footer'
 import Header from './Header'
@@ -7,11 +7,14 @@ import Main from './Main'
 
 type Props = {
   title?: string
-  home?: boolean
   children: ReactNode
 }
 
-const Layout: FC<Props> = ({ title, home, children }: Props) => {
+const Layout: FC<Props> = ({ title, children }: Props) => {
+  const viewTitle = useMemo<string>(() => {
+    return title ? `${title} | ${SITE_TITLE}` : SITE_TITLE
+  }, [title])
+
   return (
     <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">
       <Head>
@@ -23,12 +26,12 @@ const Layout: FC<Props> = ({ title, home, children }: Props) => {
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
-            SITE_TITLE
+            viewTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
-        <meta name="og:title" content={SITE_TITLE} />
+        <meta name="og:title" content={viewTitle} />
         <meta name="twitter:card" content="summary_large_image" />
-        <title>{title ? title : SITE_TITLE}</title>
+        <title>{viewTitle}</title>
       </Head>
       <Header />
       <Main>{children}</Main>
